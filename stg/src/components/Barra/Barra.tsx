b9aa25d8b4a3588d1559d2d437bbd4b85296e0a4
JhosephL <jhosephslizarazom@outlook.com>
2025-01-24 01:37:35 -0500

    :construction: Creo componentes fijos para todo el proyecto.

---
+++ stg/src/components/Barra/Barra.tsx
@@ -0,0 +1,31 @@
+
+import "./Barra.css";
+
+
+function Barra ()  {
+  
+
+  return (
+
+    <div className="barra">
+      <div className="botoon">
+        <button>Monitoreo</button>
+      </div>
+      <div className="botoon">
+        <button>Notificaciones</button>
+      </div>
+      <div className="botoon">
+        <button>Reportes</button>
+      </div>
+      <div className="botoon">
+        <button>Emparejamiento</button>
+      </div>
+    </div>
+  
+  );
+
+
+}
+
+
+export default Barra ;
---
+++ stg/src/components/Contenido/Contenido.tsx
@@ -0,0 +1,19 @@
+
+import "./Contenido.css";
+
+
+function Contenido ()  {
+  
+
+  return (
+
+    <div className="contenido"> </div>
+  
+  );
+
+
+}
+
+
+export default Contenido ;
+
