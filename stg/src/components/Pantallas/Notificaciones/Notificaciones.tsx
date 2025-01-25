fafeb07988e449f1d2708739a309daa50604e448
JhosephL <jhosephslizarazom@outlook.com>
2025-01-25 02:43:10 -0500

    :lipstick: Cambio contenido auxiliar y añado color.

--- stg/src/components/Pantallas/Notificaciones/Notificaciones.tsx
+++ stg/src/components/Pantallas/Notificaciones/Notificaciones.tsx
@@ -12,42 +12,22 @@ function Notificaciones ( ) {
       <div className="contenedor_PanelIzquierdo">
         <div className="desplazadora">
           <p>
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget erat et turpis dictum vestibulum. Cras vulputate nibh nec velit congue hendrerit. Nunc dictum metus eget dui vehicula, eget maximus arcu viverra. Nullam condimentum neque nulla, sit amet tincidunt libero tristique nec. Vivamus nunc ligula, tempus ut enim eget, faucibus finibus lorem. Morbi nec vulputate lacus. Donec imperdiet felis ac mauris venenatis malesuada. Suspendisse condimentum pretium odio in varius. Vestibulum ut eleifend ante. Phasellus euismod malesuada sem, a bibendum libero tristique eu. Maecenas ex eros, tempor vel dui eget, euismod malesuada urna. Quisque venenatis placerat dui, et suscipit augue ultricies eget. Morbi vestibulum lacus massa, sit amet varius nunc ornare quis. Duis libero arcu, mattis a varius non, facilisis et dolor. Cras a sem velit. Nam leo urna, tincidunt non pretium ac, imperdiet ac metus.
+            <br />
+            Donec elit mauris, pellentesque sit amet cursus vel, pellentesque non mauris. Maecenas ut lorem vitae dolor consequat fermentum a non ligula. Vestibulum eget mollis ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod justo et justo congue commodo. Aenean ex ligula, posuere ac imperdiet hendrerit, sodales quis velit. Nulla facilisi. Vivamus suscipit eleifend felis, luctus malesuada nulla egestas eu.
+            <br />
+            Nulla venenatis ut mauris vel blandit. Duis justo elit, consequat nec imperdiet dapibus, luctus sed orci. Nam cursus, enim sit amet consectetur interdum, ligula nunc mollis mauris, a pellentesque est sapien eget ex. Nunc sapien ipsum, maximus id lacinia cursus, egestas eget dui. Ut eu erat in nisi mollis fermentum. Duis feugiat diam purus, quis varius nisi convallis at. Aenean eu congue sem. Sed sit amet odio ante.
           </p>
         </div>
       </div>
       <div className="contenedor_PanelDerecho">
         <div className="desplazadora">
           <p>
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
-            This is some text that will overflow and create a scrollbar.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget erat et turpis dictum vestibulum. Cras vulputate nibh nec velit congue hendrerit. Nunc dictum metus eget dui vehicula, eget maximus arcu viverra. Nullam condimentum neque nulla, sit amet tincidunt libero tristique nec. Vivamus nunc ligula, tempus ut enim eget, faucibus finibus lorem. Morbi nec vulputate lacus. Donec imperdiet felis ac mauris venenatis malesuada. Suspendisse condimentum pretium odio in varius. Vestibulum ut eleifend ante. Phasellus euismod malesuada sem, a bibendum libero tristique eu. Maecenas ex eros, tempor vel dui eget, euismod malesuada urna. Quisque venenatis placerat dui, et suscipit augue ultricies eget. Morbi vestibulum lacus massa, sit amet varius nunc ornare quis. Duis libero arcu, mattis a varius non, facilisis et dolor. Cras a sem velit. Nam leo urna, tincidunt non pretium ac, imperdiet ac metus.
+            <br />
+            Donec elit mauris, pellentesque sit amet cursus vel, pellentesque non mauris. Maecenas ut lorem vitae dolor consequat fermentum a non ligula. Vestibulum eget mollis ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod justo et justo congue commodo. Aenean ex ligula, posuere ac imperdiet hendrerit, sodales quis velit. Nulla facilisi. Vivamus suscipit eleifend felis, luctus malesuada nulla egestas eu.
+            <br />
+            Nulla venenatis ut mauris vel blandit. Duis justo elit, consequat nec imperdiet dapibus, luctus sed orci. Nam cursus, enim sit amet consectetur interdum, ligula nunc mollis mauris, a pellentesque est sapien eget ex. Nunc sapien ipsum, maximus id lacinia cursus, egestas eget dui. Ut eu erat in nisi mollis fermentum. Duis feugiat diam purus, quis varius nisi convallis at. Aenean eu congue sem. Sed sit amet odio ante.
           </p>
         </div>
       </div>
