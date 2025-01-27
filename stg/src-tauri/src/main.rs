68b96cd0345351fad9733c92023af8a55146c2bb
JhosephL <jhosephslizarazom@outlook.com>
2025-01-27 00:04:33 -0500

    :construction: Se cargan datos de un XLSX en local.

--- stg/src-tauri/src/main.rs
+++ stg/src-tauri/src/main.rs
@@ -1,15 +1,42 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use calamine::{open_workbook, Reader, Xlsx, XlsxError};
+use serde::Serialize;
+
+
+
+#[derive(Serialize)]
+struct RowData {
+    value: String,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn read_xlsx_file() -> Result<Vec<RowData>, String> {
+
+    let path = "/home/user/Downloads/new.xlsx" ;
+    let mut workbook: Xlsx<_> = open_workbook(path).map_err(|e: XlsxError| e.to_string())? ;
+
+    let range = workbook
+        .worksheet_range("Sheet1")
+        .map_err(|e: XlsxError| e.to_string())? ;
+
+    let mut data = Vec::new();
+
+    for row in range.rows() {
+        if let Some(cell) = row.get(0) {
+            data.push(RowData {
+                value: cell.to_string(),
+            });
+        }
+    }
+
+Ok(data)
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![read_xlsx_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- stg/src/components/Pantallas/Notificaciones/Notificaciones.tsx
+++ stg/src/components/Pantallas/Notificaciones/Notificaciones.tsx
@@ -4,7 +4,7 @@ import "./Notificaciones.css" ;
 
 
 function Notificaciones ( ) {
-  
+
 
   return (
 
