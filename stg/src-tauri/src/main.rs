1022dfab005ca221dd5471a127e91f56be0d9b3d
JhosephL <jhosephslizarazom@outlook.com>
2025-01-27 02:05:13 -0500

    :construction: Cada panel de ´Monitoreo´ y ´Notificaciones´ lee desde XLSX.

--- stg/src-tauri/src/main.rs
+++ stg/src-tauri/src/main.rs
@@ -7,36 +7,135 @@ use serde::Serialize;
 
 
 #[derive(Serialize)]
-struct RowData {
-    value: String,
+struct DatosMonitoreoIzq {
+    id : String ,
+    rol : String ,
+    contacto : String
 }
 
 #[tauri::command]
-fn read_xlsx_file() -> Result<Vec<RowData>, String> {
+fn monitoreo_izquierdo()  ->  Result <Vec<DatosMonitoreoIzq>,String> {
 
-    let path = "/home/user/Downloads/new.xlsx" ;
-    let mut workbook: Xlsx<_> = open_workbook(path).map_err(|e: XlsxError| e.to_string())? ;
+    let path = "/home/user/Downloads/new 1.xlsx" ;
+    let mut workbook: Xlsx<_> = open_workbook(path).map_err ( |e:XlsxError| e.to_string() )? ;
 
     let range = workbook
-        .worksheet_range("Sheet1")
-        .map_err(|e: XlsxError| e.to_string())? ;
+        .worksheet_range ( "Sheet1" )
+        .map_err ( |e:XlsxError| e.to_string() )? ;
 
-    let mut data = Vec::new();
+    let mut data = Vec::new() ;
 
-    for row in range.rows() {
+    for row in range.rows ( ) {
+
+        let id = row.get(0).map_or ( "".to_string(),|cell|cell.to_string() ) ;
+        let rol = row.get(1).map_or ( "".to_string(),|cell|cell.to_string() ) ;
+        let contacto = row.get(2).map_or ( "".to_string(),|cell|cell.to_string() ) ;
+
+        data.push ( DatosMonitoreoIzq { id,rol,contacto } ) ;
+
+    }
+
+Ok(data)
+}
+
+
+#[derive(Serialize)]
+struct DatosMonitoreoDer {
+    registro: String,
+}
+
+#[tauri::command]
+fn monitoreo_derecho()  ->  Result <Vec<DatosMonitoreoDer>,String> {
+
+    let path = "/home/user/Downloads/new 2.xlsx" ;
+    let mut workbook: Xlsx<_> = open_workbook(path).map_err ( |e:XlsxError| e.to_string() )? ;
+
+    let range = workbook
+        .worksheet_range ( "Sheet1" )
+        .map_err ( |e:XlsxError| e.to_string() )? ;
+
+    let mut data = Vec::new() ;
+    
+    for row in range.rows ( ) {
         if let Some(cell) = row.get(0) {
-            data.push(RowData {
-                value: cell.to_string(),
-            });
+            data.push ( DatosMonitoreoDer {
+                registro:cell.to_string() ,
+            }) ;
         }
     }
 
 Ok(data)
 }
 
-fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![read_xlsx_file])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+#[derive(Serialize)]
+struct DatosNotificacionesIzq {
+    asunto: String,
+    contactos: String,
 }
+
+#[tauri::command]
+fn notificaciones_izquierda()  ->  Result <Vec<DatosNotificacionesIzq>,String> {
+
+    let path = "/home/user/Downloads/new 3.xlsx" ;
+    let mut workbook: Xlsx<_> = open_workbook(path).map_err ( |e:XlsxError| e.to_string() )? ;
+
+    let range = workbook
+        .worksheet_range ( "Sheet1" )
+        .map_err ( |e:XlsxError| e.to_string() )? ;
+
+    let mut data = Vec::new() ;
+
+    for row in range.rows() {
+
+        let asunto = row.get(0).map_or("".to_string(), |cell| cell.to_string());
+        let contactos = row.get(1).map_or("".to_string(), |cell| cell.to_string());
+
+        data.push ( DatosNotificacionesIzq { asunto,contactos } ) ;
+
+    }
+
+Ok(data)
+}
+
+
+#[derive(Serialize)]
+struct DatosNotificacionesDer {
+    registro: String,
+}
+
+#[tauri::command]
+fn notificaciones_derecha()  ->  Result <Vec<DatosNotificacionesDer>,String> {
+
+    let path = "/home/user/Downloads/new 4.xlsx" ;
+    let mut workbook: Xlsx<_> = open_workbook(path).map_err ( |e:XlsxError| e.to_string() )? ;
+
+    let range = workbook
+        .worksheet_range ( "Sheet1" )
+        .map_err ( |e:XlsxError| e.to_string() )? ;
+
+    let mut data = Vec::new() ;
+    
+    for row in range.rows ( ) {
+        if let Some(cell) = row.get(0) {
+            data.push ( DatosNotificacionesDer {
+                registro:cell.to_string() ,
+            }) ;
+        }
+    }
+
+Ok(data)
+}
+
+
+fn main ( ) {
+    tauri::Builder::default ( )
+        .invoke_handler ( tauri::generate_handler! [
+            monitoreo_izquierdo ,
+            monitoreo_derecho ,
+            notificaciones_izquierda ,
+            notificaciones_derecha
+        ])
+        .run ( tauri::generate_context!() )
+        .expect ( "error while running tauri application" ) ;
+}
+
--- stg/src/components/Pantallas/Monitoreo/Monitoreo.tsx
+++ stg/src/components/Pantallas/Monitoreo/Monitoreo.tsx
@@ -1,23 +1,39 @@
 
 import "./Monitoreo.css" ;
 
-import { useEffect, useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
+import { useEffect, useState } from "react" ;
+import { invoke } from "@tauri-apps/api/tauri" ;
 
-interface RowData {
-  value: string;
+
+
+interface DatosMonitoreoIzq {
+  id : string ;
+  rol : string ;
+  contacto : string ;
+}
+interface DatosMonitoreoDer {
+  registro : string ;
 }
 
 function Monitoreo ( ) {
 
-  const [data, setData] = useState<RowData[]>([]);
+  const [datosIzq,setDatosIzq] = useState<DatosMonitoreoIzq[]>([]);
 
-  useEffect(() => {
+  useEffect ( () => {
     // Fetch data from the backend
-    invoke<RowData[]>("read_xlsx_file")
-      .then((response) => setData(response))
-      .catch((error) => console.error("Failed to fetch data:", error));
-  }, []);
+    invoke<DatosMonitoreoIzq[]>("monitoreo_izquierdo")
+      .then ( (response) => setDatosIzq(response) )
+      .catch ( (error) => console.error("Failed to fetch data:", error) ) ;
+  }, [] ) ;
+
+  const [datosDer,setDatosDer] = useState<DatosMonitoreoDer[]>([]);
+
+  useEffect ( () => {
+    // Fetch data from the backend
+    invoke<DatosMonitoreoDer[]>("monitoreo_derecho")
+      .then ( (response) => setDatosDer(response) )
+      .catch ( (error) => console.error("Failed to fetch data:", error) ) ;
+  }, [] ) ;
   
 
   return (
@@ -25,20 +41,24 @@ function Monitoreo ( ) {
     <div className="monitoreo">
       <div className="contenedor_PanelIzquierdo">
         <div className="desplazadora">
-          {data.map((row, index) => (
-            <p key={index}>{row.value}</p>
+          {datosIzq.map((row, index) => (
+            <div key={index} className="casilla">
+              <div className="rootulo">
+                <p className="id">{row.id}</p>
+                <p className="rol">{row.rol}</p>
+              </div>
+              <p className="contacto">{row.contacto}</p>
+            </div>
           ))}
         </div>
       </div>
       <div className="contenedor_PanelDerecho">
         <div className="desplazadora">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget erat et turpis dictum vestibulum. Cras vulputate nibh nec velit congue hendrerit. Nunc dictum metus eget dui vehicula, eget maximus arcu viverra. Nullam condimentum neque nulla, sit amet tincidunt libero tristique nec. Vivamus nunc ligula, tempus ut enim eget, faucibus finibus lorem. Morbi nec vulputate lacus. Donec imperdiet felis ac mauris venenatis malesuada. Suspendisse condimentum pretium odio in varius. Vestibulum ut eleifend ante. Phasellus euismod malesuada sem, a bibendum libero tristique eu. Maecenas ex eros, tempor vel dui eget, euismod malesuada urna. Quisque venenatis placerat dui, et suscipit augue ultricies eget. Morbi vestibulum lacus massa, sit amet varius nunc ornare quis. Duis libero arcu, mattis a varius non, facilisis et dolor. Cras a sem velit. Nam leo urna, tincidunt non pretium ac, imperdiet ac metus.
-            <br />
-            Donec elit mauris, pellentesque sit amet cursus vel, pellentesque non mauris. Maecenas ut lorem vitae dolor consequat fermentum a non ligula. Vestibulum eget mollis ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod justo et justo congue commodo. Aenean ex ligula, posuere ac imperdiet hendrerit, sodales quis velit. Nulla facilisi. Vivamus suscipit eleifend felis, luctus malesuada nulla egestas eu.
-            <br />
-            Nulla venenatis ut mauris vel blandit. Duis justo elit, consequat nec imperdiet dapibus, luctus sed orci. Nam cursus, enim sit amet consectetur interdum, ligula nunc mollis mauris, a pellentesque est sapien eget ex. Nunc sapien ipsum, maximus id lacinia cursus, egestas eget dui. Ut eu erat in nisi mollis fermentum. Duis feugiat diam purus, quis varius nisi convallis at. Aenean eu congue sem. Sed sit amet odio ante.
-          </p>
+          {datosDer.slice(0).reverse().map((row, index) => (
+            <div className="registro">
+              <p key={index}>{row.registro}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
