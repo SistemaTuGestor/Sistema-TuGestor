12000fe118e0a9f0984e7271f3610a6ea9558fb4
Nicolas Quintana <140460204+Nikoquin1@users.noreply.github.com>
2025-02-05 15:31:09 -0500

    :cat: Actualizar hoover en la barra de módulos

--- stg/src/components/Barra/Barra.tsx
+++ stg/src/components/Barra/Barra.tsx
@@ -1,43 +1,55 @@
-
 import "./Barra.css";
-
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
+function Barra() {
+  const [mostrarContenido, setMostrarContenido] = useState(false);
 
-
-function Barra ( )  {
-  
+  const toggleContenido = () => {
+    setMostrarContenido(!mostrarContenido);
+  };
 
   return (
-
     <div className="barra">
       <div className="botoon">
-        <NavLink to="/monitoreo" className={({ isActive }) => (isActive ? "active" : "")}>
+        <NavLink 
+          to="/monitoreo" 
+          className={({ isActive }) => (isActive ? "active-button" : "")}
+        >
           <button>Monitoreo</button>
         </NavLink>
       </div>
       <div className="botoon">
-        <NavLink to="/notificaciones" className={({ isActive }) => (isActive ? "active" : "")}>
+        <NavLink 
+          to="/notificaciones" 
+          className={({ isActive }) => (isActive ? "active-button" : "")}
+        >
           <button>Notificaciones</button>
         </NavLink>
       </div>
       <div className="botoon">
-        <NavLink to="/reportes" className={({ isActive }) => (isActive ? "active" : "")}>
+        <NavLink 
+          to="/reportes" 
+          className={({ isActive }) => (isActive ? "active-button" : "")}
+        >
           <button>Reportes</button>
         </NavLink>
       </div>
       <div className="botoon">
-        <NavLink to="/emparejamiento" className={({ isActive }) => (isActive ? "active" : "")}>
+        <NavLink 
+          to="/emparejamiento" 
+          className={({ isActive }) => (isActive ? "active-button" : "")}
+        >
           <button>Emparejamiento</button>
         </NavLink>
       </div>
+
+      {/* Contenido emergente */}
+      <div className={`contenido-hover ${mostrarContenido ? "mostrar" : ""}`}>
+        Este es el contenido emergente al presionar "Monitoreo".
+      </div>
     </div>
-  
-  ) ;
-
-
+  );
 }
 
-
-export default Barra ;
-
+export default Barra;
