58fa00502f8c9fac53c7d4c8cefd2ebb38e8c7b8
JhosephL <jhosephslizarazom@outlook.com>
2025-02-05 21:29:05 -0500

    :construction: Agrego opciones al módulo de Notificaciones.

--- stg/src/components/Barra/Barra.tsx
+++ stg/src/components/Barra/Barra.tsx
@@ -1,55 +1,57 @@
-import "./Barra.css";
-import { NavLink } from "react-router-dom";
-import { useState } from "react";
 
-function Barra() {
-  const [mostrarContenido, setMostrarContenido] = useState(false);
+import "./Barra.css" ;
 
-  const toggleContenido = () => {
-    setMostrarContenido(!mostrarContenido);
-  };
+import { NavLink } from "react-router-dom" ;
+
+
+function Barra ( ) {
 
   return (
+
     <div className="barra">
+
       <div className="botoon">
         <NavLink 
           to="/monitoreo" 
-          className={({ isActive }) => (isActive ? "active-button" : "")}
+          className={({ isActive }) => (isActive ? "botoon-activo" : "")}
         >
           <button>Monitoreo</button>
         </NavLink>
       </div>
+      
       <div className="botoon">
         <NavLink 
           to="/notificaciones" 
-          className={({ isActive }) => (isActive ? "active-button" : "")}
+          className={({ isActive }) => (isActive ? "botoon-activo" : "")}
         >
           <button>Notificaciones</button>
         </NavLink>
       </div>
+
       <div className="botoon">
         <NavLink 
           to="/reportes" 
-          className={({ isActive }) => (isActive ? "active-button" : "")}
+          className={({ isActive }) => (isActive ? "botoon-activo" : "")}
         >
           <button>Reportes</button>
         </NavLink>
       </div>
+
       <div className="botoon">
         <NavLink 
           to="/emparejamiento" 
-          className={({ isActive }) => (isActive ? "active-button" : "")}
+          className={({ isActive }) => (isActive ? "botoon-activo" : "")}
         >
           <button>Emparejamiento</button>
         </NavLink>
       </div>
 
-      {/* Contenido emergente */}
-      <div className={`contenido-hover ${mostrarContenido ? "mostrar" : ""}`}>
-        Este es el contenido emergente al presionar "Monitoreo".
-      </div>
     </div>
-  );
+
+  ) ;
+
 }
 
-export default Barra;
+
+export default Barra ;
+
--- stg/src/components/Pantallas/Notificaciones/Notificaciones.tsx
+++ stg/src/components/Pantallas/Notificaciones/Notificaciones.tsx
@@ -19,7 +19,7 @@ function Notificaciones ( ) {
   const [datosIzq,setDatosIzq] = useState<DatosNotificacionesIzq[]>([]);
   
     useEffect ( () => {
-      // Fetch data from the backend
+      // Fetch data from the backend.
       invoke<DatosNotificacionesIzq[]> ( "notificaciones_izquierda" )
         .then ( (response) => setDatosIzq(response) )
         .catch ( (error) => console.error("Failed to fetch data:", error) ) ;
@@ -28,7 +28,7 @@ function Notificaciones ( ) {
     const [datosDer,setDatosDer] = useState<DatosNotificacionesDer[]>([]) ;
   
     useEffect ( () => {
-      // Fetch data from the backend
+      // Fetch data from the backend.
       invoke<DatosNotificacionesDer[]> ( "notificaciones_derecha" )
         .then ( (response) => setDatosDer(response) )
         .catch ( (error) => console.error("Failed to fetch data:", error) ) ;
@@ -40,7 +40,7 @@ function Notificaciones ( ) {
     <div className="notificaciones">
       <div className="contenedor_PanelIzquierdo">
         <div className="desplazadora">
-          {datosIzq.map((row, index) => (
+          {datosIzq.map((row,index) => (
             <div key={index} className="casilla">
               <p className="asunto">{row.asunto}</p>
               <p className="contactos">{row.contactos}</p>
@@ -49,6 +49,20 @@ function Notificaciones ( ) {
         </div>
       </div>
       <div className="contenedor_PanelDerecho">
+        <div className="opciones">
+          <select multiple className="botones-opciones">
+            <option value="destinatarios">Destinatario</option>
+            <option value="opt-2">Destinatario 2</option>
+            <option value="opt-3">Destinatario 3</option>
+          </select>
+          <select className="botones-opciones">
+            <option value="objetos">Objetos</option>
+            <option value="opt-2">Opción 2</option>
+            <option value="opt-3">Opción 3</option>
+            <option value="opt-4">Opción 4</option>
+            <option value="opt-5">Opción 5</option>
+          </select>
+        </div>
         <div className="desplazadora">
           {datosDer.map((row, index) => (
             <div className="registro">
