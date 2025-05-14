544fb0ec969ec0e435447bf13085d4717b216dcb
Nicolas Quintana <140460204+Nikoquin1@users.noreply.github.com>
2025-05-14 12:26:06 -0500

    🧑‍🎓Generar los la estructura de los mensajes junto con las Apis

--- stg/src-tauri/src/notificaciones/whatsapp.rs
+++ stg/src-tauri/src/notificaciones/whatsapp.rs
@@ -4,6 +4,7 @@ use serde_json::Value;
 use std::fs;
 use std::path::{Path, PathBuf};
 use xlsxwriter::{Workbook, Format};
+use urlencoding::encode;
 use crate::notificaciones::leer_archivos::{
     TutoresPUJ, TutoresColegio, FuncionariosColegio, TutoradosEmparejados, TutoradosControl,
     leer_archivo_emparejados, leer_archivo_control, generar_tutores, generar_tutores_enlaces,
@@ -15,7 +16,6 @@ struct Borrador {
     asunto: String,
     mensaje: String,
 }
-
 
 #[derive(Serialize, Debug)]
 pub struct TutoresPUJwhatsapp {
@@ -47,7 +47,6 @@ pub struct FuncionariosColegiowhatsapp {
     correo: String,
     telefono: Vec<String>,
     institucion: String,
-    
 }
 
 #[derive(Serialize, Debug)]
@@ -68,7 +67,6 @@ pub struct TutoradosEmparejadoswhatsapp {
     e: String,
     f: String,
     g: String,
-
 }
 
 #[derive(Serialize, Debug)]
@@ -90,50 +88,15 @@ pub struct TutoradosControlwhatsapp {
     f: String,
     g: String,
 }
-
-
-/*
-#[tauri::command]
-pub fn generar_excel_whatsapp() -> Result<String, String> {
-    let fecha_actual = "2025-05-13".to_string();
-    let nombre_archivo = format!("Historial_{}.xlsx", fecha_actual);
-
-    let workbook = Workbook::new(&nombre_archivo).map_err(|e| e.to_string())?;
-    let mut worksheet = workbook.add_worksheet(None).map_err(|e| e.to_string())?;
-
-    let header_format = Format::new().set_bold();
-
-    // Escribir encabezados
-    worksheet.write_string(0, 0, "Nombre", Some(&header_format))?;
-    worksheet.write_string(0, 1, "Apellido", Some(&header_format))?;
-    worksheet.write_string(0, 2, "Correo", Some(&header_format))?;
-    worksheet.write_string(0, 3, "Institución", Some(&header_format))?;
-    worksheet.write_string(0, 4, "Teléfono", Some(&header_format))?;
-    worksheet.write_string(0, 5, "Horas", Some(&header_format))?;
-    worksheet.write_string(0, 6, "Tutorados", Some(&header_format))?;
-    worksheet.write_string(0, 7, "Link", Some(&header_format))?;
-
-    // Leer datos desde las funciones de `leer_archivos`
-    let (tutores_puj, _, _, _) = leer_archivo_emparejados().map_err(|e| format!("Error al leer emparejados: {}", e))?;
-
-    // Escribir datos de Tutores PUJ
-    let mut row = 1;
-    for tutor in tutores_puj {
-        worksheet.write_string(row, 0, &tutor.nombre, None)?;
-        worksheet.write_string(row, 1, &tutor.apellido, None)?;
-        worksheet.write_string(row, 2, &tutor.correo, None)?;
-        worksheet.write_string(row, 3, &tutor.institucion, None)?;
-        worksheet.write_string(row, 4, &tutor.telefono.join(", "), None)?;
-        worksheet.write_string(row, 5, &tutor.horas, None)?;
-        worksheet.write_string(row, 6, &tutor.tutorados.join(", "), None)?;
-        worksheet.write_string(row, 7, &tutor.link, None)?;
-        row += 1;
-    }
-
-    workbook.close().map_err(|e| e.to_string())?;
-    Ok(nombre_archivo)
+#[derive(Serialize, Debug)]
+pub struct Mensaje {
+    correo: String,
+    numero: String,
+    nombre: String,
+    asunto: String,
+    mensaje: String,
+    link: String, // Nuevo campo
 }
-*/
 
 #[tauri::command]
 pub fn procesar_datos_para_whatsapp() -> Result<
@@ -151,7 +114,6 @@ pub fn procesar_datos_para_whatsapp() -> Result<
 
     let tutorados_control = leer_archivo_control().map_err(|e| format!("Error al leer control: {}", e))?;
 
-    // Convertir los datos a las estructuras de `whatsapp.rs`
     let tutores_puj_whatsapp: Vec<TutoresPUJwhatsapp> = tutores_puj
         .into_iter()
         .map(|tutor| TutoresPUJwhatsapp {
@@ -234,20 +196,12 @@ pub fn procesar_datos_para_whatsapp() -> Result<
         })
         .collect();
 
-    /* 
-    println!("Tutores PUJ: {:?}", tutores_puj_whatsapp);
-    println!("Tutores Colegio: {:?}", tutores_colegio_whatsapp);
-    println!("Funcionarios Colegio: {:?}", funcionarios_colegio_whatsapp);
-    println!("Tutorados Emparejados: {:?}", tutorados_emparejados_whatsapp);
-    println!("Tutorados Control: {:?}", tutorados_control_whatsapp);
-*/
     Ok((
         tutores_puj_whatsapp,
         tutores_colegio_whatsapp,
         funcionarios_colegio_whatsapp,
         tutorados_emparejados_whatsapp,
         tutorados_control_whatsapp,
-
     ))
 }
 
@@ -256,50 +210,46 @@ fn get_resource_path() -> PathBuf {
     let current_exe = std::env::current_exe().expect("Failed to get current executable path");
     let mut path = current_exe.parent().unwrap().to_path_buf();
 
-    // Navegar hacia arriba hasta encontrar la carpeta "Sistema-TuGestor"
     while !path.ends_with("Sistema-TuGestor") && path.parent().is_some() {
         path = path.parent().unwrap().to_path_buf();
     }
 
-    // Agregar la carpeta "recursos"
     path.push("recursos");
     path
 }
 
 #[tauri::command]
-pub fn procesar_mensajes_desde_json() -> Result<Vec<String>, String> {
-   // println!("Procesando mensajes desde JSON...1");
+pub fn procesar_mensajes_desde_json() -> Result<Vec<Mensaje>, String> {
+    let (
+        tutores_puj_whatsapp,
+        tutores_colegio_whatsapp,
+        funcionarios_colegio_whatsapp,
+        tutorados_emparejados_whatsapp,
+        tutorados_control_whatsapp,
+    ) = procesar_datos_para_whatsapp()?;
 
-    // Llamar a `procesar_datos_para_whatsapp` para obtener los datos procesados
-    let (tutores_puj_whatsapp, tutores_colegio_whatsapp, funcionarios_colegio_whatsapp, tutorados_emparejados_whatsapp, tutorados_control_whatsapp) =
-        procesar_datos_para_whatsapp()?;
-//println!("Datos procesados correctamente.2");
-    // Construir la ruta del archivo JSON
     let base_path = get_resource_path();
     let path = base_path.join("historiales").join("historial.json");
-//println!("Ruta del archivo 2");
-    // Leer el archivo JSON
-    let contenido = std::fs::read_to_string(&path).map_err(|e| format!("Error al leer el archivo JSON: {}", e))?;
-    let mensajes: Value = serde_json::from_str(&contenido).map_err(|e| format!("Error al parsear el JSON: {}", e))?;
-//println!("Archivo JSON leído y parseado correctamente.3");
+    let contenido = std::fs::read_to_string(&path)
+        .map_err(|e| format!("Error al leer el archivo JSON: {}", e))?;
+    let mensajes: Value =
+        serde_json::from_str(&contenido).map_err(|e| format!("Error al parsear el JSON: {}", e))?;
+
     let mut mensajes_generados = Vec::new();
 
-    // Procesar los mensajes
     if let Some(mensajes_array) = mensajes.as_array() {
-       // println!("Mensajes encontrados en el JSON.4");
-       // println!("Mensajes encontrados: {:?}", mensajes_array);
         for mensaje in mensajes_array {
-           // println!("Mensajes dentro del array.5");
-           // println!("Mensaje encontrado: {:?}", mensaje);
             if mensaje["estado"].as_bool().unwrap_or(true) {
                 let destinatarios_default = Vec::new();
                 let destinatarios = mensaje["destinatarios"].as_array().unwrap_or(&destinatarios_default);
                 let mensaje_plantilla = mensaje["mensaje"].as_str().unwrap_or("");
+                let asunto = mensaje["asunto"].as_str().unwrap_or("").to_string();
 
                 for destinatario in destinatarios {
                     if let Some(destinatario_str) = destinatario.as_str() {
                         match destinatario_str {
                             "TutoresPUJ" => {
+                                println!("Procesando mensajes para TutoresPUJ");
                                 for tutor in &tutores_puj_whatsapp {
                                     let mensaje_real = mensaje_plantilla
                                         .replace("<<nombre TutoresPUJ>>", &tutor.nombre)
@@ -309,12 +259,26 @@ pub fn procesar_mensajes_desde_json() -> Result<Vec<String>, String> {
                                         .replace("<<telefono TutoresPUJ>>", &tutor.telefono.join(", "))
                                         .replace("<<horas TutoresPUJ>>", &tutor.horas)
                                         .replace("<<tutorados TutoresPUJ>>", &tutor.tutorados.join(", "));
-                                    println!("🧑‍🎓🧑‍🏫TutoresPUJ");
-                                    println!("Mensaje real: {}", mensaje_real);
-                                    mensajes_generados.push(mensaje_real);
+                                    let numero = tutor.telefono.get(0).cloned().unwrap_or_default();
+                                    let nombre = format!("{} {}", tutor.nombre, tutor.apellido);
+                                    let encoded_message = encode(&mensaje_real).to_string();
+                                    let whatsapp_url = format!(
+                                        "https://api.whatsapp.com/send?phone={}&text={}",
+                                        numero, encoded_message
+                                    );
+                                    mensajes_generados.push(Mensaje {
+                                        correo: tutor.correo.clone(),
+                                        numero: numero.clone(),
+                                        nombre: nombre.clone(),
+                                        asunto: asunto.clone(),
+                                        mensaje: mensaje_real.clone(),
+                                        link: whatsapp_url.clone(),
+                                    });
+                                    println!("Mensaje generado para TutoresPUJ: {:?}", mensajes_generados.last());
                                 }
                             }
                             "TutoresColegio" => {
+                                println!("Procesando mensajes para TutoresColegio");
                                 for tutor in &tutores_colegio_whatsapp {
                                     let mensaje_real = mensaje_plantilla
                                         .replace("<<nombre TutoresColegio>>", &tutor.nombre)
@@ -324,24 +288,54 @@ pub fn procesar_mensajes_desde_json() -> Result<Vec<String>, String> {
                                         .replace("<<telefono TutoresColegio>>", &tutor.telefono.join(", "))
                                         .replace("<<horas TutoresColegio>>", &tutor.horas)
                                         .replace("<<tutorados TutoresColegio>>", &tutor.tutorados.join(", "));
-                                    println!("🏫👳‍♂️TutoresColegio");
-                                    println!("Mensaje real: {}", mensaje_real);
-                                    mensajes_generados.push(mensaje_real);
+                                    let numero = tutor.telefono.get(0).cloned().unwrap_or_default();
+                                    let nombre = format!("{} {}", tutor.nombre, tutor.apellido);
+                                    let encoded_message = encode(&mensaje_real).to_string();
+                                    let whatsapp_url = format!(
+                                        "https://api.whatsapp.com/send?phone={}&text={}",
+                                        numero, encoded_message
+                                    );
+
+                                    mensajes_generados.push(Mensaje {
+                                        correo: tutor.correo.clone(),
+                                        numero: numero.clone(),
+                                        nombre: nombre.clone(),
+                                        asunto: asunto.clone(),
+                                        mensaje: mensaje_real.clone(),
+                                        link: whatsapp_url.clone(),
+                                    });
+                                    println!("Mensaje generado: {:?}", mensajes_generados.last());
                                 }
                             }
                             "FuncionariosColegio" => {
+                                println!("Procesando mensajes para FuncionariosColegio");
                                 for funcionario in &funcionarios_colegio_whatsapp {
                                     let mensaje_real = mensaje_plantilla
                                         .replace("<<nombre FuncionariosColegio>>", &funcionario.nombre)
                                         .replace("<<correo FuncionariosColegio>>", &funcionario.correo)
                                         .replace("<<telefono FuncionariosColegio>>", &funcionario.telefono.join(", "))
                                         .replace("<<institucion FuncionariosColegio>>", &funcionario.institucion);
-                                    println!("🏫Funcionarios Colegio");
-                                    println!("Mensaje real: {}", mensaje_real);
-                                    mensajes_generados.push(mensaje_real);
+                                    let numero = funcionario.telefono.get(0).cloned().unwrap_or_default();
+                                    let nombre = funcionario.nombre.clone();
+                                    let encoded_message = encode(&mensaje_real).to_string();
+                                    let whatsapp_url = format!(
+                                        "https://api.whatsapp.com/send?phone={}&text={}",
+                                        numero, encoded_message
+                                    );
+
+                                    mensajes_generados.push(Mensaje {
+                                        correo: funcionario.correo.clone(),
+                                        numero: numero.clone(),
+                                        nombre: nombre.clone(),
+                                        asunto: asunto.clone(),
+                                        mensaje: mensaje_real.clone(),
+                                        link: whatsapp_url.clone(),
+                                    });
+                                    println!("Mensaje generado: {:?}", mensajes_generados.last());
                                 }
                             }
                             "TutoradosEmparejados" => {
+                                println!("Procesando mensajes para TutoradosEmparejados");
                                 for tutorado in &tutorados_emparejados_whatsapp {
                                     let mensaje_real = mensaje_plantilla
                                         .replace("<<nombre TutoradosEmparejados>>", &tutorado.nombre)
@@ -360,12 +354,27 @@ pub fn procesar_mensajes_desde_json() -> Result<Vec<String>, String> {
                                         .replace("<<e TutoradosEmparejados>>", &tutorado.e)
                                         .replace("<<f TutoradosEmparejados>>", &tutorado.f)
                                         .replace("<<g TutoradosEmparejados>>", &tutorado.g);
-                                    println!("🥪Tutorados Emparejados");
-                                    println!("Mensaje real: {}", mensaje_real);
-                                    mensajes_generados.push(mensaje_real);
+                                    let numero = tutorado.telefono.get(0).cloned().unwrap_or_default();
+                                    let nombre = tutorado.nombre.clone();
+                                    let encoded_message = encode(&mensaje_real).to_string();
+                                    let whatsapp_url = format!(
+                                        "https://api.whatsapp.com/send?phone={}&text={}",
+                                        numero, encoded_message
+                                    );
+
+                                    mensajes_generados.push(Mensaje {
+                                        correo: tutorado.correo.clone(),
+                                        numero: numero.clone(),
+                                        nombre: nombre.clone(),
+                                        asunto: asunto.clone(),
+                                        mensaje: mensaje_real.clone(),
+                                        link: whatsapp_url.clone(),
+                                    });
+                                    println!("Mensaje generado: {:?}", mensajes_generados.last());
                                 }
                             }
                             "TutoradosControl" => {
+                                println!("Procesando mensajes para TutoradosControl");
                                 for tutorado in &tutorados_control_whatsapp {
                                     let mensaje_real = mensaje_plantilla
                                         .replace("<<nombre TutoradosControl>>", &tutorado.nombre)
@@ -384,9 +393,23 @@ pub fn procesar_mensajes_desde_json() -> Result<Vec<String>, String> {
                                         .replace("<<e TutoradosControl>>", &tutorado.e)
                                         .replace("<<f TutoradosControl>>", &tutorado.f)
                                         .replace("<<g TutoradosControl>>", &tutorado.g);
-                                    println!("🔑Tutorados Contol");
-                                    println!("Mensaje real: {}", mensaje_real);
-                                    mensajes_generados.push(mensaje_real);
+                                    let numero = tutorado.telefono.get(0).cloned().unwrap_or_default();
+                                    let nombre = tutorado.nombre.clone();
+                                    let encoded_message = encode(&mensaje_real).to_string();
+                                    let whatsapp_url = format!(
+                                        "https://api.whatsapp.com/send?phone={}&text={}",
+                                        numero, encoded_message
+                                    );
+
+                                    mensajes_generados.push(Mensaje {
+                                        correo: tutorado.correo.clone(),
+                                        numero: numero.clone(),
+                                        nombre: nombre.clone(),
+                                        asunto: asunto.clone(),
+                                        mensaje: mensaje_real.clone(),
+                                        link: whatsapp_url.clone(),
+                                    });
+                                    println!("Mensaje generado: {:?}", mensajes_generados.last());
                                 }
                             }
                             _ => {
